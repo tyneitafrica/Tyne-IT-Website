70ffcf66236654844187b36e889411e4662b58ae
torobucci <kevintoro254@gmail.com>
2024-09-20 12:19:36 +0300

    update navbar

--- src/components/child-components/DropDown.tsx
+++ src/components/child-components/DropDown.tsx
@@ -1,37 +1,74 @@
-import { Dropdown } from "react-bootstrap"
-import { NavLink } from "react-router-dom"
+import { useState } from "react";
+import { Dropdown } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+// import { MenuOpenContext } from "./Navbar";
 
 type linkToChildrenProps = {
-    title:string,
-    link:string,
-    children?:linkToChildrenProps[]
-}
-export const DropdownComponent = ({linkTo,linkToChildren}:{linkTo:string,linkToChildren:linkToChildrenProps[]})=>{
-   return (
-    <Dropdown as="li">
-        <Dropdown.Toggle
-          as={NavLink}
-        to={linkTo} // Change from button to div
-          style={{
-            width: "100%",
-          }}
-          variant="success"
-          id="dropdown-basic"
-        >
-            {linkTo}
-        </Dropdown.Toggle>
+  title: string;
+  link: string;
+  children?: linkToChildrenProps[];
+};
+export const DropdownComponent = ({
+  linkTo,
+  linkToChildren,
+  overflow,
+  setOverflow,
+}: {
+  linkTo: string;
+  linkToChildren: linkToChildrenProps[];
+  overflow: boolean;
+  setOverflow: React.Dispatch<React.SetStateAction<boolean>>;
+}) => {
+  const [show, setShow] = useState(false);
+//   const { isMenuOpen,setIsMenuOpen } = useContext(MenuOpenContext);
 
-        <Dropdown.Menu as="ul" className="w-100" style={{ padding: "3px 9px" }}>
-            {linkToChildren.map((linkToChild,index)=>{
-                return (
-                    <li key={index}>
-                        {!linkToChild.children? <Dropdown.Item as={NavLink} to={linkToChild.link}>
-                            {linkToChild.title}
-                        </Dropdown.Item>:<DropdownComponent linkTo={linkToChild.title} linkToChildren={linkToChild.children}/>}
-                    </li>
-                )
-            })}
-        </Dropdown.Menu>
-      </Dropdown>
-   )
-}
+  const handleMouseEnter = () => {
+    setShow(true);
+    setOverflow(true);
+  };
+  const handleMouseLeave = () => {
+    setShow(false);
+    setOverflow(false);
+  };
+  return (
+    <Dropdown
+      as="li"
+      show={show}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <Dropdown.Toggle
+        as={NavLink}
+        to={linkTo} // Change from button to div
+        style={{
+          width: "100%",
+        }}
+        variant="success"
+        id="dropdown-basic"
+      >
+        {linkTo}
+      </Dropdown.Toggle>
+
+      <Dropdown.Menu as="ul" className="w-100" style={{ padding: "3px 9px" }}>
+        {linkToChildren.map((linkToChild, index) => {
+          return (
+            <li key={index}>
+              {!linkToChild.children ? (
+                <Dropdown.Item as={NavLink} to={linkToChild.link}>
+                  {linkToChild.title}
+                </Dropdown.Item>
+              ) : (
+                <DropdownComponent
+                  linkTo={linkToChild.title}
+                  linkToChildren={linkToChild.children}
+                  overflow={overflow}
+                  setOverflow={setOverflow}
+                />
+              )}
+            </li>
+          );
+        })}
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+};
--- src/components/child-components/Navbar.tsx
+++ src/components/child-components/Navbar.tsx
@@ -1,36 +1,55 @@
-import tyneLogo from '../../assets/img/tyne.ico'
-import { NavLink } from 'react-router-dom'
-import { Fade as Hamburger } from "hamburger-react"
-import { useState } from 'react'
-import { MobileNav } from './MobileNav'
-import { DeskNav } from './DeskNav'
-export const NavBar = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false)
-  return (
-    <header id="header" className="header d-flex align-items-center fixed-top">
-      <div className="container-fluid container-xl position-relative d-flex align-items-center">
+import tyneLogo from "../../assets/img/tyne.ico";
+import { NavLink } from "react-router-dom";
+import { Fade as Hamburger } from "hamburger-react";
+import { useState } from "react";
+import { MobileNav } from "./MobileNav";
+import { DeskNav } from "./DeskNav";
+import { createContext } from "react";
 
-        <NavLink to="/" className="logo d-flex align-items-center me-auto">
-          <h1 className="sitename"><img src={tyneLogo} alt="tyneIT Logo" /></h1>
-        </NavLink>
-
-        <nav id="navmenu" className="navmenu">
-          <DeskNav/>
-          <div className='d-xl-none'>
-        <Hamburger
-            toggled={isMenuOpen}
-            toggle={setIsMenuOpen}
-            direction="right"
-            size={28}
-            color="#666"
-            
-          />
-        </div>
-        </nav>
-        <MobileNav isMenuOpen={isMenuOpen}/>
-
-        <a className="btn-getstarted" href="#about">Get Started</a>
-      </div>
-    </header>
-  )
+interface MenuOpenContextType {
+  isMenuOpen: boolean;
+  setIsMenuOpen: (value: boolean) => void;
 }
+
+export const MenuOpenContext = createContext<MenuOpenContextType>({
+  isMenuOpen: false,
+  setIsMenuOpen: () => {
+  },
+});
+export const NavBar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  return (
+    <MenuOpenContext.Provider value={{ isMenuOpen, setIsMenuOpen }}>
+      <header
+        id="header"
+        className="header d-flex align-items-center fixed-top"
+      >
+        <div className="container-fluid container-xl position-relative d-flex align-items-center">
+          <NavLink to="/" className="logo d-flex align-items-center me-auto">
+            <h1 className="sitename">
+              <img src={tyneLogo} alt="tyneIT Logo" />
+            </h1>
+          </NavLink>
+
+          <nav id="navmenu" className="navmenu">
+            <DeskNav />
+            <div className="d-xl-none">
+              <Hamburger
+                toggled={isMenuOpen}
+                toggle={setIsMenuOpen}
+                direction="right"
+                size={28}
+                color="#666"
+              />
+            </div>
+          </nav>
+          <MobileNav />
+
+          <a className="btn-getstarted" href="#about">
+            Get Started
+          </a>
+        </div>
+      </header>
+    </MenuOpenContext.Provider>
+  );
+};
